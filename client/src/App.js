154260d8a8164e9962dfe70c8e27fa836b0fb3ca
kenny oh <kennyoh517@gmail.com>
2020-06-30 17:35:54 -0400

    remove inventory component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,20 @@
 import React from 'react'
 
-import Checkout from './components/Checkout'
-import Inventory from './components/Inventory'
+// import Checkout from './components/Checkout'
 import ShoppingCart from './components/ShoppingCart'
 import { IconButton, Badge } from '@material-ui/core'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const axios = require('axios')
-
-// xxx: make api call of all items with quantity > 0
-const sampleInventory = [
-  {
-    quantity: 1,
-    id: 1,
-    name: 'foo',
-  },
-  {
-    quantity: 2,
-    id: 2,
-    name: 'bar',
-  },
-]
+const assign = require('lodash/assign')
+const find = require('lodash/find')
+const findIndex = require('lodash/findIndex')
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
+
     this.state = {
       inventory: [],
       cart: [],
@@ -33,26 +22,58 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    // xxx: make api call of all items with quantity > 0
     axios.get('/api/inventory')
-      .then(({data}) => this.setState((state, props) => ({inventory : data})))
+      .then(({data}) => {
+        const inventory = data.map(x => assign({}, x, {selectionCount: 0}))
+
+        this.setState((state, props) => ({ inventory }))
+      })
   }
 
   addToCart = selections => {
-    const cart = selections.filter(x => x.count > 0)
+    const cart = selections.filter(x => x.selectionCount > 0)
 
     this.setState({cart})
   }
 
+  updateCount = (id, change) => {
+    let item = find(this.state.inventory, x => x.id === id)
+
+    if (change === 'increment') {
+      item.selectionCount++
+    } else if (change === 'decrement') {
+      item.selectionCount--
+    }
+
+    const itemIndex = findIndex(this.state.inventory, x => x.id === item.id)
+    // overwrite item at index with new count
+    this.state.inventory[itemIndex] = item
+
+    this.setState(this.state.inventory);
+  }
+
   render() {
+    const items = this.state.inventory.map(x =>
+      <div key={x.id}>
+        <p onClick={() => this.updateCount(x.id, 'decrement')}> 🔽 </p>
+        <p>{`${x.name} ${x.selectionCount}`}</p>
+        <p onClick={() => this.updateCount(x.id, 'increment')}> 🔼 </p>
+      </div>
+    )
+
     return (
       <div className='App'>
         <ShoppingCart cart={this.state.cart}/>
         <IconButton aria-label="cart">
-          <Badge badgeContent={this.state.cart.reduce((acc, cur) => acc + cur.count, 0)} color="secondary">
+          <Badge badgeContent={this.state.cart.reduce((acc, cur) => acc + cur.selectionCount, 0)} color="secondary">
             <ShoppingCartIcon />
           </Badge>
         </IconButton>
-        <Inventory addToCart={this.addToCart} inventory={this.state.inventory} />
+        <p>inventoary</p>
+        {items}
+
+        <button onClick={() => this.addToCart(this.state.inventory)}>Add to cart</button>
       </div>
     )
   }
