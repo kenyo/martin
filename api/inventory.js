7bbda3d186e5fba41c9cf9147f6364135aa33709
kenny oh <kennyoh517@gmail.com>
2020-07-05 22:08:11 -0400

    add console.log

--- api/inventory.js
+++ api/inventory.js
@@ -4,6 +4,7 @@ const pool = require('../config')
 const getBooks = (request, response) => {
   pool.query('SELECT * FROM inventory', (error, results) => {
     if (error) {
+      console.log(process.env.DATABASE_URL)
       throw error
     }
     response.status(200).json(results.rows)
