80832bd9c0be55f0a59ed4537955d25f3e598ac5
kenny oh <kennyoh517@gmail.com>
2021-02-05 15:55:27 -0500

    change inventory to stateless functional component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 // import Checkout from './components/Checkout'
 import ShoppingCart from './components/ShoppingCart'
-import { IconButton, Badge } from '@material-ui/core'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Inventory from './components/Inventory'
 
-const axios = require('axios')
-const assign = require('lodash/assign')
-const find = require('lodash/find')
-const findIndex = require('lodash/findIndex')
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 
 export default class App extends React.Component {
 
@@ -21,60 +18,20 @@ export default class App extends React.Component {
     }
   }
 
-  componentDidMount() {
-    axios.get('/api/inventory')
-      .then(({data}) => {
-        const inventory = data.map(x => assign({}, x, {selectionCount: 0}))
-
-        this.setState((state, props) => ({ inventory }))
-      })
-  }
-
-  addToCart = selections => {
-    const cart = selections.filter(x => x.selectionCount > 0)
-
-    this.setState({cart})
-  }
-
-  updateCount = (id, change) => {
-    let item = find(this.state.inventory, x => x.id === id)
-
-    if (change === 'increment') {
-      item.selectionCount++
-    } else if (change === 'decrement') {
-      item.selectionCount--
-    }
-
-    const itemIndex = findIndex(this.state.inventory, x => x.id === item.id)
-    const inventory = this.state.inventory
-    // overwrite item at index with new count
-    inventory[itemIndex] = item
-
-    this.setState((state, props) => ({ inventory }))
-  }
-
   render() {
-    const items = this.state.inventory.map(x =>
-      <div key={x.id}>
-        <p onClick={() => this.updateCount(x.id, 'decrement')}> 🔽 </p>
-        <p>{`${x.name} ${x.selectionCount}`}</p>
-        <p onClick={() => this.updateCount(x.id, 'increment')}> 🔼 </p>
-      </div>
-    )
 
     return (
-      <div className='App'>
-        <ShoppingCart cart={this.state.cart}/>
-        <IconButton aria-label="cart">
-          <Badge badgeContent={this.state.cart.reduce((acc, cur) => acc + cur.selectionCount, 0)} color="secondary">
-            <ShoppingCartIcon />
-          </Badge>
-        </IconButton>
-        <p>inventory</p>
-        {items}
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Inventory />
+          </Route>
+          <Route path='/cart'>
+            <ShoppingCart cart={this.state.cart} />
+          </Route>
 
-        <button onClick={() => this.addToCart(this.state.inventory)}>Add to cart</button>
-      </div>
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ client/src/components/Inventory.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import { IconButton, Badge } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+const axios = require('axios')
+const assign = require('lodash/assign')
+const find = require('lodash/find')
+const findIndex = require('lodash/findIndex')
+
+const { useState, useEffect } = require('react');
+
+export default function Inventory() {
+  const [cart, setCart] = useState([]);
+  const [inventory, setInventory] = useState([])
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    axios.get('/api/inventory')
+      .then(({data}) => {
+        const inventory = data.map(x => assign({}, x, {selectionCount: 0}))
+
+        setInventory(inventory)
+        // this.setState((state, props) => ({ inventory }))
+      })
+  });
+
+  const items = inventory.map(x =>
+    <div key={x.id}>
+      <p onClick={() => this.updateCount(x.id, 'decrement')}> 🔽 </p>
+      <p>{`${x.name} ${x.selectionCount}`}</p>
+      <p onClick={() => this.updateCount(x.id, 'increment')}> 🔼 </p>
+    </div>
+  )
+
+  return (
+    <div className='App'>
+      <h2>martin's bakery</h2>
+      <IconButton aria-label="cart">
+        <Badge badgeContent={cart.reduce((acc, cur) => acc + cur.selectionCount, 0)} color="secondary">
+          <Link to='/cart'><ShoppingCartIcon /></Link>
+        </Badge>
+      </IconButton>
+      <p>inventory</p>
+      {items}
+
+      <button onClick={() => this.addToCart(this.state.inventory)}>Add to cart</button>
+    </div>
+  )
+}
+
+// export default class Inventory extends React.Component {
+
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       inventory: [],
+//       cart: [],
+//     }
+//   }
+
+//   componentDidMount() {
+//     axios.get('/api/inventory')
+//       .then(({data}) => {
+//         const inventory = data.map(x => assign({}, x, {selectionCount: 0}))
+
+//         this.setState((state, props) => ({ inventory }))
+//       })
+//   }
+
+//   addToCart = selections => {
+//     const cart = selections.filter(x => x.selectionCount > 0)
+
+//     this.setState((state, props) => ({ cart }))
+//   }
+
+//   updateCount = (id, change) => {
+//     let item = find(this.state.inventory, x => x.id === id)
+
+//     if (change === 'increment') {
+//       item.selectionCount++
+//     } else if (change === 'decrement') {
+//       item.selectionCount--
+//     }
+
+//     const itemIndex = findIndex(this.state.inventory, x => x.id === item.id)
+//     const inventory = this.state.inventory
+//     // overwrite item at index with new count
+//     inventory[itemIndex] = item
+
+//     this.setState((state, props) => ({ inventory }))
+//   }
+
+//   render() {
+//     const items = this.state.inventory.map(x =>
+//       <div key={x.id}>
+//         <p onClick={() => this.updateCount(x.id, 'decrement')}> 🔽 </p>
+//         <p>{`${x.name} ${x.selectionCount}`}</p>
+//         <p onClick={() => this.updateCount(x.id, 'increment')}> 🔼 </p>
+//       </div>
+//     )
+
+//     return (
+//       <div className='App'>
+//         <h2>martin's bakery</h2>
+//         <IconButton aria-label="cart">
+//           <Badge badgeContent={this.state.cart.reduce((acc, cur) => acc + cur.selectionCount, 0)} color="secondary">
+//             <Link to='/cart'><ShoppingCartIcon /></Link>
+//           </Badge>
+//         </IconButton>
+//         <p>inventory</p>
+//         {items}
+
+//         <button onClick={() => this.addToCart(this.state.inventory)}>Add to cart</button>
+//       </div>
+//     )
+//   }
+// }
